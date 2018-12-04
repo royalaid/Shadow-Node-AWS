56062600e63acd5db5640dd88087b66003fa2515
Mark Aiken <gwmaiken@att.net>
2018-12-04 12:07:00 -0800

    Initial Lambda repro

--- src/server/main.cljs
+++ src/server/main.cljs
@@ -5,6 +5,10 @@
 
 (defonce value-b 2)
 
+(defn handler []
+  #js{:statusCode 200
+      :body (js/JSON.stringify "Hello from Shadow")})
+
 (defn reload! []
   (println "Code updated.")
   (println "Trying values:" value-a value-b))
