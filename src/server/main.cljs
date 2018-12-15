c7291d91cafbb414834916ef901c6b8990dafc6e
Mark Aiken <gwmaiken@att.net>
2018-12-14 19:19:48 -0800

    First pass of get item dynamo

--- src/server/main.cljs
+++ src/server/main.cljs
@@ -1,5 +1,30 @@
+(ns server.main
+  (:require ["aws-sdk" :as AWS]))
 
-(ns server.main)
+(AWS/config.update #js{:region "us-east-1"})
+
+(def dynamo (AWS/DynamoDB.  #js{:apiVersion "2012-08-10"}))
+
+(def params (clj->js {:TableName "increamental-clicker-counts"
+                      :Key {"user-id" {:S "0"}}}))
+
+(def intro #js{#_#_:dynamo dynamo
+               :res (fn []
+                      (.getItem dynamo params
+                                (fn [e d]
+                                  (println d))))})
+
+
+(println "Start")
+(.getItem dynamo params
+          (fn [e d]
+            (println "DONE!")
+            (def err (or e "nothing"))
+            (def data d)))
+(println "Done")
+
+(println "Yo Foo")
+(js/console.log "CC")
 
 (defn handler [_ _ cb]
   (cb nil
